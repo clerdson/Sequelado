55750eaa9890b40a97a7702d8a4ce5f1bd944858
clerdson <clerdsonj@gmail.com>
2020-07-30 20:25:08 -0300

    connect database and select database with c

--- Sequelado.c
+++ Sequelado.c
@@ -1,14 +1,76 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <gtk/gtk.h>
 #include <libpq-fe.h>
+#include <stdlib.h>
 
-
-
+void do_exit(PGconn *conn){
+PQfinish(conn);
+exit(1);
+}
 
 int main() {
 
-int lib_ver = PQlibVersion();
-printf("Version of libpq:%d\n",lib_ver);    
+PGresult *res;
+int nFields; 
+int i,j;
+ 
+PGconn *conn =PQconnectdb("host=localhost user=postgres dbname=mydb password=root");
+
+if(PQstatus(conn)==CONNECTION_BAD){
+printf("connection to database failed:%s\n",PQerrorMessage(conn));
+
+do_exit(conn);
+}
+int ver = PQserverVersion(conn);
+printf("Server version %d\n",ver);
+/*
+*Start with begin
+*/
+res = PQexec(conn,"BEGIN");
+if(PQresultStatus(res)!=PGRES_COMMAND_OK)
+{
+printf("BEGIN command failed :%s",PQerrorMessage(conn));
+}
+
+/*
+*create select in table 
+*/
+res = PQexec(conn,"declare myportal cursor for select*from casa");
+if(PQresultStatus(res)!=PGRES_COMMAND_OK){
+printf("erro ocorreu in %s \n",PQerrorMessage(conn));
+}
+
+res = PQexec(conn,"fetch all in myportal");
+if(PQresultStatus(res)!=PGRES_TUPLES_OK){
+printf("fethc all falho %s",PQerrorMessage(conn));
+}
+/*
+*primeiroimprimir os nomes dos atributos
+*/
+
+nFields = PQnfields(res);
+for(i=0;i<nFields;i++){
+printf(" | %s |",PQfname(res,i));
+}
+printf ("\n\n");
+printf("LINHAS \n");
+/*
+*next,print rows
+*/
+for (i=0;i<PQntuples(res);i++){
+for(j=0;j<nFields;j++){
+printf("--------\n");
+printf("%s",PQgetvalue(res,i,j));
+printf("\n");
+}
+printf("\n");
+}
+
+
+
+
+PQclear(res);
+PQfinish(conn);
 
 
 return 0;
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 all : Sequelado.c
-	gcc Sequelado.c -o base `pkg-config --cflags --libs gtk+-3.0` -I/usr/include/postgresql -L/usr/include/postgres -lpq
-   
+	gcc Sequelado.c -o Sequelado `pkg-config --cflags --libs gtk+-3.0` -I/usr/include/postgresql -L/usr/include/postgres -lpq
+
