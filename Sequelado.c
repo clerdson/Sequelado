b674ef6d05f098cbdb7d8bbba05018de1d6061b1
clerdson <clerdsonj@gmail.com>
2020-07-30 16:32:43 -0300

    make file

--- Sequelado.c
+++ Sequelado.c
@@ -1,31 +1,15 @@
+#include<stdio.h>
 #include <gtk/gtk.h>
-int main(int argc, char *argv[]){
-
-GtkWidget *window;
-GtkWidget *button;
-GtkWidget *halign;
-
-gtk_init(&argc ,&argv);
-window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-gtk_window_set_title(GTK_WINDOW(window),"Sequelado");
-gtk_window_set_default_size(GTK_WINDOW(window),400,400); 
+#include <libpq-fe.h>
 
 
 
 
-button = gtk_button_new_with_label("Sequelado");
-gtk_widget_set_tooltip_text(button,"Sequelado");
+int main() {
 
-halign = gtk_alignment_new (0,0,0,0);
-gtk_container_add(GTK_CONTAINER(halign),button);
-gtk_container_add(GTK_CONTAINER(window),halign);
+int lib_ver = PQlibVersion();
+printf("Version of libpq:%d\n",lib_ver);    
 
-
-gtk_widget_show_all(window);
-
-
-
-gtk_main();
 
 return 0;
 }
