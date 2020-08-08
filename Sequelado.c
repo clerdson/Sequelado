d5364d2fa7d0e02ca2ada4d79b9a9d35a859f33b
clerdson <clerdsonj@gmail.com>
2020-08-07 21:54:29 -0300

    toolbar

--- Sequelado.c
+++ Sequelado.c
@@ -5,6 +5,7 @@
 #include "do_exit.h"
 #include "create.h"
 
+
 //void do_exit(PGconn *conn){
 //PQfinish(conn);
 //exit(1);
@@ -12,9 +13,20 @@
 
 void create(PGresult *res,PGconn *conn);
 
+
+
 int main(int argc,char *argv[]) {
 
-GtkWidget *window ,*label;
+  GtkWidget *window;
+  GtkWidget *vbox;
+  
+  GtkWidget *toolbar;
+  GtkToolItem *newTb;
+  GtkToolItem *openTb;
+  GtkToolItem *saveTb;
+  GtkToolItem *sep;
+  GtkToolItem *exitTb;
+
 PGresult *res;
 int nFields; 
 int i,j;
@@ -96,18 +108,40 @@ gtk_init(&argc,&argv);
 
 window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 gtk_window_set_title(GTK_WINDOW(window),"Sequelado");
+gtk_container_set_border_width(GTK_CONTAINER(window),10);
+gtk_widget_set_size_request(window,200,200);
 
 
-label=gtk_label_new("Hello World");
-gtk_label_set_selectable(GTK_LABEL(label),TRUE);
+ vbox = gtk_vbox_new(FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
 
-gtk_container_add(GTK_CONTAINER(window),label);
+  toolbar = gtk_toolbar_new();
+  gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
+
+  newTb = gtk_tool_button_new_from_stock(GTK_STOCK_NEW);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), newTb, -1);
+
+  openTb = gtk_tool_button_new_from_stock(GTK_STOCK_OPEN);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), openTb, -1);
+
+  saveTb = gtk_tool_button_new_from_stock(GTK_STOCK_SAVE);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), saveTb, -1);
+
+  sep = gtk_separator_tool_item_new();
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), sep, -1); 
+
+  exitTb = gtk_tool_button_new_from_stock(GTK_STOCK_QUIT);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), exitTb, -1);
+
+  gtk_box_pack_start(GTK_BOX(vbox), toolbar, FALSE, FALSE, 5);
+
+  g_signal_connect(G_OBJECT(exitTb), "clicked", 
+        G_CALLBACK(gtk_main_quit), NULL);
+
+  g_signal_connect(G_OBJECT(window), "destroy",
+        G_CALLBACK(gtk_main_quit), NULL);
+
 gtk_widget_show_all(window);
-
-
-
-
-
 
 //finish PQ and GTK
 gtk_main();
@@ -117,3 +151,5 @@ PQfinish(conn);
 
 return 0;
 }
+
+
